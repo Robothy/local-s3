1d4bfcb955f7e2df8fb8469d0262ac888c3865d8
Fuxiang Luo <robothyluo@gmail.com>
2025-07-05 22:52:52 +0800

    remove unused code.

--- local-s3-core/src/main/java/com/robothy/s3/core/util/JsonUtils.java
+++ local-s3-core/src/main/java/com/robothy/s3/core/util/JsonUtils.java
@@ -6,9 +6,6 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
 import lombok.SneakyThrows;
 
 public class JsonUtils {
@@ -24,26 +21,14 @@ public class JsonUtils {
 
   private static final ObjectWriter jsonWriter = jsonMapper.writer();
 
-  private static final ObjectWriter prettyJsonWriter = jsonMapper.writer().withDefaultPrettyPrinter();
-
   @SneakyThrows
   public static <T> T fromJson(String json, Class<T> clazz) {
     return jsonReader.readValue(json, clazz);
   }
 
   @SneakyThrows
-  public static <T> T fromJson(URL url, Class<T> clazz) {
-    return jsonReader.readValue(url, clazz);
-  }
-
-  @SneakyThrows
   public static <T> T fromJson(File jsonFile, Class<T> clazz) {
     return jsonReader.readValue(jsonFile, clazz);
-  }
-
-  @SneakyThrows
-  public static <T> T fromJson(InputStream jsonInputStream, Class<T> clazz) {
-    return jsonReader.readValue(jsonInputStream, clazz);
   }
 
   @SneakyThrows
@@ -54,26 +39,6 @@ public class JsonUtils {
   @SneakyThrows
   public static void toJson(File destFile, Object object) {
     jsonWriter.writeValue(destFile, object);
-  }
-
-  @SneakyThrows
-  public static void toJson(OutputStream out, Object object) {
-    jsonWriter.writeValue(out, object);
-  }
-
-  @SneakyThrows
-  public static String toPrettyJson(Object object) {
-    return prettyJsonWriter.writeValueAsString(object);
-  }
-
-  @SneakyThrows
-  public static void toPrettyJson(File destFile, Object object) {
-    prettyJsonWriter.writeValue(destFile, object);
-  }
-
-  @SneakyThrows
-  public static void toPrettyJson(OutputStream out, Object object) {
-    prettyJsonWriter.writeValue(out, object);
   }
 
 }
