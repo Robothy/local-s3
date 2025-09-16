5b1117c7f1a7bf3cc30ebadc8df4036c6fd317d9
Fuxiang Luo <robothyluo@gmail.com>
2025-09-16 12:17:15 +0800

    fix tests.

--- local-s3-jupiter/src/test/java/com/robothy/s3/jupiter/extensions/LocalS3EndpointResolverTest.java
+++ local-s3-jupiter/src/test/java/com/robothy/s3/jupiter/extensions/LocalS3EndpointResolverTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import java.net.URI;
@@ -15,26 +16,29 @@ import java.net.URI;
 @LocalS3
 class LocalS3EndpointResolverTest {
 
-  @Order(1)
-  @Test
-  void test1(LocalS3Endpoint endpoint) {
-    S3Client client = S3Client.builder()
+  S3Client createS3Client(LocalS3Endpoint endpoint) {
+    return S3Client.builder()
         .endpointOverride(URI.create("http://localhost:" + endpoint.port()))
         .forcePathStyle(true)
         .region(Region.AP_EAST_1)
+        .credentialsProvider(AnonymousCredentialsProvider.create())
         .build();
-    assertDoesNotThrow(() -> client.createBucket(builder -> builder.bucket("my-bucket")));
+  }
+
+  @Order(1)
+  @Test
+  void test1(LocalS3Endpoint endpoint) {
+    try(S3Client client = createS3Client(endpoint)) {
+      assertDoesNotThrow(() -> client.createBucket(builder -> builder.bucket("my-bucket")));
+    }
   }
 
   @Order(2)
   @Test
   void test2(LocalS3Endpoint endpoint) {
-    S3Client client = S3Client.builder()
-        .endpointOverride(URI.create("http://localhost:" + endpoint.port()))
-        .forcePathStyle(true)
-        .region(Region.AP_EAST_1)
-        .build();
-    assertDoesNotThrow(() -> client.headBucket(builder -> builder.bucket("my-bucket")));
+    try(S3Client client = createS3Client(endpoint)) {
+      assertDoesNotThrow(() -> client.headBucket(builder -> builder.bucket("my-bucket")));
+    }
   }
 
 }
--- local-s3-jupiter/src/test/java/com/robothy/s3/jupiter/extensions/ResolveBeforeAllParametersTest.java
+++ local-s3-jupiter/src/test/java/com/robothy/s3/jupiter/extensions/ResolveBeforeAllParametersTest.java
@@ -4,7 +4,6 @@ import com.robothy.s3.jupiter.LocalS3;
 import com.robothy.s3.jupiter.LocalS3Endpoint;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -14,39 +13,26 @@ import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
 import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @LocalS3
 public class ResolveBeforeAllParametersTest {
 
-  private static LocalS3Endpoint endpoint;
-
-  private static S3Client s3Client;
-
   @BeforeAll
   static void setup(LocalS3Endpoint endpoint, S3Client s3Client) throws Exception {
-    ResolveBeforeAllParametersTest.endpoint = endpoint;
-    ResolveBeforeAllParametersTest.s3Client = s3Client;
 
     s3Client.createBucket(b -> b.bucket("my-bucket"));
 
-    S3Client clientWithInjectedEndpoint = S3Client.builder().region(Region.of("local"))
+    try (S3Client clientWithInjectedEndpoint = S3Client.builder().region(Region.of("local"))
       .endpointOverride(new URI(endpoint.endpoint()))
       .forcePathStyle(true)
       .credentialsProvider(AnonymousCredentialsProvider.create())
-      .build();
-    assertDoesNotThrow(() -> clientWithInjectedEndpoint.headBucket(HeadBucketRequest.builder().bucket("my-bucket").build()));
-  }
-
-  @Test
-  void testInjectedInstancesShareTheSameEndpoint(LocalS3Endpoint endpoint, S3Client s3Client) {
-    assertEquals(ResolveBeforeAllParametersTest.endpoint, endpoint);
-    assertDoesNotThrow(() -> s3Client.headBucket(HeadBucketRequest.builder().bucket("my-bucket").build()));
+      .build()) {
+      assertDoesNotThrow(() -> clientWithInjectedEndpoint.headBucket(HeadBucketRequest.builder().bucket("my-bucket").build()));
+    }
   }
 
   @AfterAll
-  static void cleanup(LocalS3Endpoint endpoint, S3Client s3Client) {
-    assertEquals(ResolveBeforeAllParametersTest.endpoint, endpoint);
+  static void cleanup(S3Client s3Client) {
     s3Client.deleteBucket(DeleteBucketRequest.builder().bucket("my-bucket").build());
   }
 
